63fee15dde20675cd3eb42b1cdc94066a2a3f9a6
unknown <kseniyakit@gmail.com>
2019-11-27 17:07:23 +0200

    commit42

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,10 +1,9 @@
-let m = 10;
-let n = 20;
+const m = 10;
+const n = 20;
 let result = 1;
-do{
-    if(m % 2 ===1){
-        result += m;
-    }
-    m++
-} while (m<=n);
-console.log('Result: ' + result);
+for(let i=m; i<=n; i++){
+    if(i % 2 ===1);
+        result += i;
+        console.log(result);
+}
+
