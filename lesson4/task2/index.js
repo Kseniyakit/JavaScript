07773b98844e83150777aaa55110320a883c8690
unknown <kseniyakit@gmail.com>
2019-11-27 17:14:49 +0200

    commit45

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,9 +1,13 @@
 const m = 10;
 const n = 20;
 let result = 1;
-for(let i=m; i<=n; i++){
-    if(i % 2 ===1);
+let i = m;
+
+do {
+    if (i % 2 === 1) {
         result *= i;
-        console.log(result);
-}
+    }
+    i++;
+} while (i <= n);
+
 console.log('Result: ' + result);
