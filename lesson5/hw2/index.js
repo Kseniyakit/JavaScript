50ac3bfbe20018a2511fce29a72050f2fb926588
Kseniyakit <kseniyakit@gmail.com>
2019-11-29 18:15:01 +0200

    commit72

--- lesson5/hw2/index.js
+++ lesson5/hw2/index.js
@@ -1,9 +1,12 @@
 function getPrimes(n) {
+    let sum = 0;
     nextPrime: for (let i = 2; i < n; i++) {
  
       for (let j = 2; j < i; j++) {
         if (i % j == 0) continue nextPrime;
       }
+       sum =i;
+      console.log (sum); 
     }
   }
  
