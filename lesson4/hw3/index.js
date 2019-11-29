88c39b2e574209534203ae01856c49f4c2f67de0
unknown <kseniyakit@gmail.com>
2019-11-29 10:55:30 +0200

    commit56

--- lesson4/hw3/index.js
+++ lesson4/hw3/index.js
@@ -1,12 +1,12 @@
 let result =0;
 for (let i=0; i<=1000;i++ ){
-if (i%2 != 0){
+if (i % 2 != 0){
     console.log('Found');
     result +=1;
 }
 }
-    if (result*5 >5000){
-        console.log('Bigger');
+    if (result * 5 > 5000){
+        console.log("Bigger");
     }  else {
-        console.log('Smaller or equal')
+        console.log("Smaller or equal");
     }
