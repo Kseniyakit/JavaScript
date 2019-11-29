206ddae05d0a57512f0962ea2536df153a73569e
Kseniyakit <kseniyakit@gmail.com>
2019-11-29 19:06:49 +0200

    commit77

--- lesson5/hw5/index.js
+++ lesson5/hw5/index.js
@@ -1,9 +1,8 @@
 function findDivCount(a, b, n){
    let result = 0;
     for (let i =a; i<=b; i++){
-        if (i%n===0) {
-            result++;
-        }
+        if (i%n===0) console.log (result++);
+        
      return result;
     }
 }
