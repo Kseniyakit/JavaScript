bfc87b1420680e1621e99635677ed57301231457
unknown <kseniyakit@gmail.com>
2019-11-29 10:51:24 +0200

    commit55

---
+++ lesson4/hw3/index.js
@@ -0,0 +1,12 @@
+let result =0;
+for (let i=0; i<=1000;i++ ){
+if (i%2 != 0){
+    console.log('Found');
+    result +=1;
+}
+}
+    if (result*5 >5000){
+        console.log('Bigger');
+    }  else {
+        console.log('Smaller or equal')
+    }
