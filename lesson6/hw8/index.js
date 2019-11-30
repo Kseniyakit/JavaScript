724e66fbffaa50214935e7fae62d0d36606c0141
Kseniyakit <kseniyakit@gmail.com>
2019-11-30 23:03:07 +0200

    commit109

--- lesson6/hw8/index.js
+++ lesson6/hw8/index.js
@@ -1,7 +1,4 @@
 function cloneArr(arr){
-    copy = arr.slice(0);
-		for (i = 0; i < copy.length; i++) {
-			copy[i] = cloneArr(copy[i]);
-		}
-		return copy;
+    const array = [...arr];
+    return array;
 }
