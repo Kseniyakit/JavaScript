658d261c55a6c22cf6400cbac4f9dbbb1163ddf1
Kseniyakit <kseniyakit@gmail.com>
2019-11-30 22:35:37 +0200

    commit100

---
+++ lesson6/hw7/index.js
@@ -0,0 +1,27 @@
+
+let arr = [2, 5, 6, 1200, 12, 15, 0, 3, -8, 9];
+let max = getMaxValue(arr);
+let min = getMinValue(arr);
+
+ 
+
+function getMaxValue(arr){
+    let max = arr[0]; 
+    for (let i = 0; i < arr.length; i++) { 
+        if (max < arr[i]) max = arr[i]; 
+    }
+    return max;
+}
+function getMinValue(arr){
+    let min = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (min > arr[i]) min = arr[i];
+    }
+    return min;
+}
+function checker(arr){
+    let sum = max + min;
+    if (sum<1000) return false
+    else return true;
+}
+console.log(checker(arr));
