3703b501c79591d826f99f8c401ccef418669573
Kseniyakit <kseniyakit@gmail.com>
2019-11-30 15:11:33 +0200

    commit80

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,7 +1,10 @@
 const numbers = [4, 7, 99,30];
 
 function getArrayBounds(arr) {
+    if(!Array.isArray(arr)){
+        return null;
+    }
 return [arr.length, arr[0], arr[arr.length-1]];
-}
+} 
 const arrayBound = getArrayBounds(numbers);
 console.log(arrayBound);
