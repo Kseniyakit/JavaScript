0e526cfa7210f4e67a3fcee61f7d87d6709c4570
Kseniyakit <kseniyakit@gmail.com>
2019-11-30 20:53:19 +0200

    commit94

--- lesson6/hw3/index.js
+++ lesson6/hw3/index.js
@@ -1,6 +1,6 @@
 function sortAsc(array){
-    for (i=0; i<array.length; i++){
-    for (j=0; j<array.length; j++){
+    for (let i=0; i<array.length; i++){
+    for (let j=0; j<array.length; j++){
         if (array[i]<array[j]){
            let bols = array[i];
            array [i]= array [j];
@@ -11,8 +11,8 @@ function sortAsc(array){
 } 
 
 function sortDes(array){
-    for (i=0; i<array.length; i++){
-    for (j=0; j<array.length; j++){
+    for (let i=0; i<array.length; i++){
+    for (let j=0; j<array.length; j++){
         if (array[i]>array[j]){
            let bols = array[i];
            array [i]= array [j];
