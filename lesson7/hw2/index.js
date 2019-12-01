a8eb096ae5b12c2c9e7bcb3e9607088fc40ef70c
Kseniyakit <kseniyakit@gmail.com>
2019-12-01 20:16:35 +0200

    commit116

---
+++ lesson7/hw2/index.js
@@ -0,0 +1,7 @@
+const number = [7, 99, 101, 303, 88];
+const copy = number.slice();
+const reverseArray = copy => 
+  (!Array.isArray(copy))
+     ? null : copy.reverse();
+
+ console.log(reverseArray(number));
