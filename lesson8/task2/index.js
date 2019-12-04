e430fc8f6641e92ad769e1d99053551afc163c28
unknown <kseniyakit@gmail.com>
2019-12-04 17:45:38 +0200

    commit 119

---
+++ lesson8/task2/index.js
@@ -0,0 +1,14 @@
+const user = {
+    name: 'John Doe',
+    age: 17,
+    interest: 'football'
+};
+
+const concatProps = obj => {
+    let arr = [];
+    for(let key in obj){
+        arr = arr.concat(obj[key]);
+    }
+    return arr;
+}
+console.log (concatProps(user));
