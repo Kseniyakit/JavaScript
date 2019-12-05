3ab371a300f99dde5d1eb3dd29ab808576f59d93
Kseniyakit <kseniyakit@gmail.com>
2019-12-05 08:54:07 +0200

    commit120

---
+++ lesson8/hw1/index.js
@@ -0,0 +1,13 @@
+const arr = ['a', 17.1, 'John Doe', 13, 45];
+const obj = { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe', '21':21, '3':3 };
+function pickProps (obj, arr){
+    let result = {};
+    for (let i = 0; i < arr.length; i++){
+        for (let key in obj){
+            if (arr[i]==key) result[key]=obj[key];
+         
+        }
+    }
+    return result;
+} 
+console.log(pickProps(obj, arr));
