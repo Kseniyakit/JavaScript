a94e5ce4ed7d36592b9cf2bad5d378d561521149
unknown <kseniyakit@gmail.com>
2019-12-06 17:57:52 +0200

    commit124

---
+++ lesson9/hw1/index.js
@@ -0,0 +1,18 @@
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+};
+const getCustomersList = obj => {
+    const usersArray = Object.entries(obj);
+    const arr = 
+/*  const filteredUsersArray = usersArray.filter(user => 
+    if user.age[1] ) 
+  return usersArray; */
+}
+console.log(getCustomersList(customers));
