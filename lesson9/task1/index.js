0eb965d679445e5d59ac5e6a3e1a29b5ececda9a
unknown <kseniyakit@gmail.com>
2019-12-06 13:49:36 +0200

    commit122

---
+++ lesson9/task1/index.js
@@ -0,0 +1,14 @@
+const users = {
+    'John Doe': 19,
+     'Tom': 17,
+    'Bob': 18,
+};
+const getAdults = usersObj => {
+    const usersArray = Object.entries(usersObj);
+   const filteredUsersArray = usersArray
+    .filter(user => user[1] >=18);
+    const usersNames = filteredUsersArray
+    .map(user => user[0]);
+    return usersNames;
+}
+console.log(getAdults(users));
