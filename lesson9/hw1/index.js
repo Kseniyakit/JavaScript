3ce19728c05afde9a318c2c5585f1dd3a9a9d2f2
Kseniyakit <kseniyakit@gmail.com>
2019-12-07 16:12:54 +0200

    commit124

---
+++ lesson9/hw1/index.js
@@ -0,0 +1,18 @@
+const customers = {
+    'customer-id-1': {
+        name: 'William',
+        age: 54
+    },
+    'customer-id-2': {
+        name: 'Tom',
+        age: 17
+    },
+};
+function getCustomersList(obj){
+    let object = Object.assign({}, obj);
+    let arr = Object.entries(obj)
+    .map(x => x ={...x[1], id: x[0]} )
+    .sort((a,b)=>a.age > b.age ? 1 : -1);
+    return arr;
+}
+console.log(getCustomersList(customers))
