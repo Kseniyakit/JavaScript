ea19805e0a23b59ea3ebb8ce2e16537d316ca865
Kseniyakit <kseniyakit@gmail.com>
2019-12-08 22:37:57 +0200

    commit137

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,7 +1,7 @@
 const arr =  [-777, 3, -1, 45, -20];
-const getMaxAbsoluteNumber = arr =>{
-  (!Array.isArray(arr))||(arr.length === 0) ? null :
+const getMaxAbsoluteNumber = arr =>
+  (!Array.isArray(arr) || arr.length === 0) ? null :
  Math.max(...arr.map(num =>Math.abs(num)));
    
-}
+
 console.log(getMaxAbsoluteNumber(arr));
