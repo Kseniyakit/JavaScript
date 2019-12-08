ba22e30140be32db680dba40e789ac9a6f762aea
Kseniyakit <kseniyakit@gmail.com>
2019-12-08 21:54:02 +0200

    commit134

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,3 +1,3 @@
 
 const multiRound = num =>
-    [(Math.floor(num*100))/100, (Math.round(num*100))/100, (Math.ceil (num*100))/100, (Math.trunc(num*100))/100, +num.toFixed()]
+    [(Math.floor(num*100))/100, (Math.round(num*100))/100, (Math.ceil (num*100))/100, (Math.trunc(num*100))/100, Number(num.toFixed(2))]
