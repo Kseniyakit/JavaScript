7f804e153006f4f8228283252bdbc3db27aaf078
Kseniyakit <kseniyakit@gmail.com>
2019-12-09 21:43:46 +0200

    commit150

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -5,7 +5,7 @@ let max = 7.9765;
 const getRandomNumbers = (l,min,max) =>{
   const arr = [];
 
-  if (max<min || (0<(max-min)<1)){
+  if (max<min || (0<(max-min)<1) && Math.ceil(max) === Math.ceil(min)){
       return null;
   }
 for(let i=0; i<=(l-1); i++){
