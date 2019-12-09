1815e01ac69010f59bea54ea33c6c434239d233a
Kseniyakit <kseniyakit@gmail.com>
2019-12-09 22:32:07 +0200

    commit171

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -19,7 +19,7 @@ return arr;
 const randomInteger =(min, max) =>{
 min = Math.ceil(min);
  max = Math.floor(max);
-return Math.floor(Math.random() * (max - min)) + min;
+return Math.floor(Math.random() * (max - min) + min);
 };
   
 console.log(getRandomNumbers(l,min,max)); 
