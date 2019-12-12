fa4927ea61c7fa183157ee2e4ac3a20b43d66271
Kseniyakit <kseniyakit@gmail.com>
2019-12-12 08:13:34 +0200

    commit186

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -2,6 +2,6 @@ const countOccurrences = (string, stringToFind) =>{
 if (stringToFind === ''){
     return null;
 }
-return (string.split(stringToFind).length);
+return (string.split(stringToFind).length - 1);
 }
 console.log(countOccurrences('The quick brown the fox jumps over the lazy dog.', 'the'));
