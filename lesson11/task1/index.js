e8f4cd43dc3f1d0434949e4b1a3ba85ea0afc562
Kseniyakit <kseniyakit@gmail.com>
2019-12-13 08:54:30 +0200

    commit203

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -2,7 +2,7 @@ const splitText = (text, len=10) => {
     const strArr = [];
     let startPosition = 0;
     if (typeof text !== 'string') {
-        break;
+        return null;
     }
   while(len){
     let chunk = text.substr(startPosition, len);
