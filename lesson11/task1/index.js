0cb08050073b37ea90ca832053db3691d1523d19
Kseniyakit <kseniyakit@gmail.com>
2019-12-13 07:45:54 +0200

    commit188

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,19 +1,17 @@
-const splitText = (text, len) => {
-    const strArr = [];
+function splitText(str, len = 10) {
+    if (typeof str !== 'string') return null;
+    let arr = [];
     let startPosition = 0;
-
-while(true){
-    let chunk = text.substr(startPosition, len);
-    if(chunk.length === 0) {
-        break;
+    while (true) {
+        let result = str.substr(startPosition, len);
+        if (result.length === 0) {
+            break;
+        }
+        arr.push(result[0].toUpperCase() + result.slice(1));
+        startPosition += len;
     }
-  
-    strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-    startPosition += len;
-  
-    }
-    return strArr.join('\n');
-};
+    return arr.join('\n');
+}
 
 
 
