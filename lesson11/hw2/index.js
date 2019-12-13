497696f73bc94c2c54d7467faec12230fde044b4
Kseniyakit <kseniyakit@gmail.com>
2019-12-13 08:35:01 +0200

    commit 195

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -16,19 +16,3 @@ while (idx != -1) {
 }
 console.log(indices);
 
-const splitText = (text, len) => {
-  const strArr = [];
-  let startPosition = 0;
-
-while(true){
-  let chunk = text.substr(startPosition, len);
-  if(chunk.length === 0) {
-      break;
-  }
-
-  strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-  startPosition += len;
-
-  }
-  return strArr.join('\n');
-};
