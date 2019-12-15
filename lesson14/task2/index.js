21305c367396ebe02fa7de819fed265251699828
Kseniyakit <kseniyakit@gmail.com>
2019-12-15 17:32:56 +0200

    commit222

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -1,19 +1,22 @@
-const createMessanger= () => {
-	let message = "Just learn it!";
-	let sender = "Gromcode";
-	function sendMessage(name){
-		console.log(`${name}, ${message} Your ${sender}`);
-	}
-	function setMessage(text){
-		message = text;
-    }
-    function setSender(text){
-		sender = text;
-	}
-	return {
-		sendMessage,
-		setMessage,
-		setSender,
-	};
-};
+export default function createMessenger() {
+    let sender = 'Gromcode';
+    let message = 'Just learn it!';
 
+    function sendMessage(name) {
+        console.log(`${name}, ${message} Your ${sender}`);
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(sender2) {
+        sender = sender2;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    }
+}
