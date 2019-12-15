0243ac5b0c77a8ca0c600fdb6835e7a81f302aff
Kseniyakit <kseniyakit@gmail.com>
2019-12-15 16:46:09 +0200

    commit2017

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,9 +1,12 @@
+'use strict';
+
 let message = 'Just learn it!';
-function sendMessage(name){
+
+export function sendMessage(name){
     const sender = 'Gromcode';
     console.log(`${name}, ${message}. Your ${sender}`);
 }
-function setMessage(text) {
+export function setMessage(text) {
     message = text;
 }
 sendMessage('Ann');
