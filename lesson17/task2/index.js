423c1ed468cf41e87c650bb79d56cf9f90df5b89
Kseniyakit <kseniyakit@gmail.com>
2019-12-19 08:13:44 +0200

    commit235

--- lesson17/task2/index.js
+++ lesson17/task2/index.js
@@ -1,11 +1,11 @@
-const callbackPrompt = {
+export const callbackPrompt = {
     message: 'Tell me your number',
     showPrompt() {
         const phoneNumber = prompt (this.message);
         console.log(phoneNumber);
     },
     showDeferredPrompt (ms) {
-        setTimeout(this.showPrompt, ms);
+        setTimeout(() => this.showPrompt(), ms);
     }
 };
-/* callbackPrompt.showDeferredPrompt(1000); */
+callbackPrompt.showDeferredPrompt(1000); 
