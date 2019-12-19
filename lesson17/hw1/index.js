1b5769d5be5f7c8b78adc997ffa734eb6305ce1c
unknown <kseniyakit@gmail.com>
2019-12-19 11:30:59 +0200

    commit244

--- lesson17/hw1/index.js
+++ lesson17/hw1/index.js
@@ -1,13 +1,14 @@
 export const user = {
-
+    firstName: 'John',
+    secondName: 'Doe',
     getFullName() {
         return `${this.firstName} ${this.secondName}`;
        
     },
     setFullName(fullName) {
-     let arr = fullName.split(' ');
-      this.firstName = arr[0];
-      this.secondName = arr[1];
+     let arr = fullName.split(" ");
+     return this.firstName = arr[0];
+     return this.secondName = arr[1];
     },
 
 };
