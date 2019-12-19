910dcaef6228184686eba65cbf6a04c3aac11b9d
unknown <kseniyakit@gmail.com>
2019-12-19 17:31:08 +0200

    commit247

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -1,9 +1,8 @@
-function bind(func, context, ...args1){
+export function bind(func, context, ...args1){
     return function(...args2) {
 		return func.call(context, ...args2, ...args1);
 	}
 }
-
 
 
 
