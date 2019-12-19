e4399f01e8da2d270448d20a277316834ea55e87
unknown <kseniyakit@gmail.com>
2019-12-19 10:18:48 +0200

    commit236

---
+++ lesson17/task3/index.js
@@ -0,0 +1,10 @@
+function defer(func, ms) {
+    return function(){
+        setTimeout(func, ms);
+    }
+}
+
+const sayHi = () => {
+    console.log('Hi');
+}
+const deferredSayHi = defer(sayHi, 1000);
