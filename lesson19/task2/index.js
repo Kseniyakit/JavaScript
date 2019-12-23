92aea6c6dfe45974a2ad6cd7788755d1b34812ec
unknown <kseniyakit@gmail.com>
2019-12-23 14:35:06 +0200

    commit261

---
+++ lesson19/task2/index.js
@@ -0,0 +1,33 @@
+const vehicle = {
+    name: 'Argo',
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} stopped`);
+    },
+};
+
+const ship = {
+    __proto__: vehicle,
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        this.move();
+    },
+    stopMachine() {
+        console.log(`${this.name} lifting anchor down`);
+        this.stop();
+    },
+    name: 'petya',
+};
+
+const getOwnProps = (obj) => {
+    let arr = [];
+    for(let prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function')
+        arr.push(prop);
+    
+    }
+    return  arr;
+};
+getOwnProps(ship);
