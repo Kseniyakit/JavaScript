8df2690745ef7b91f8890cc4bc9f8c132fed7fb7
unknown <kseniyakit@gmail.com>
2019-12-23 15:17:19 +0200

    commit269

--- lesson19/task4/index.js
+++ lesson19/task4/index.js
@@ -1,21 +1,22 @@
 export function User(name, age) {
     this.name = name;
     this.age = age;
-    this.sayHi = function() {
+}
+    User.prototype.sayHi = function() {
         console.log(`hi, I am ${this.name}`);
     };
-    this.requestNewPhoto = function(){
+    User.prototype.requestNewPhoto = function(){
         console.log(`New photo request was sent for ${this.name}`);
     };
-    this.setAge = function(age) {
+    User.prototype.setAge = function(age) {
         if(age<0) return false;
         if(age >= 25) {
             console.log(`New photo request was sent for ${this.name}`);
         };
         this.age = age;
         return age;
-    }
-}
+    };
+
 const user1 = new User('Tom', 28);
 console.log(user1);
 user1.setAge(29);
