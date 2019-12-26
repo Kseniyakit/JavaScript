aafe705ae9c949c2b71b49cf080786cd5fcdbdfa
Kseniyakit <kseniyakit@gmail.com>
2019-12-26 15:08:22 +0200

    commit290

--- lesson22/hw1/index.js
+++ lesson22/hw1/index.js
@@ -1,9 +1,9 @@
 let tasks =   [
-    { text: 'Visit party', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Buy milk', done: false },
-    { text: 'Buy meat', done: true },
-    { text: 'Visit doctor', done: true}
+    { text: 'Visit party', done: false, date: new Date(2019,11,31), },
+    { text: 'Pick up Tom from airport', done: false, date: new Date(2019,11,26), },
+    { text: 'Buy milk', done: false, date: new Date(2020,8,9), },
+    { text: 'Buy meat', done: true, date: new Date(2019,6,16), },
+    { text: 'Visit doctor', done: true, date: new Date(2019,9,5), }
 ];
 const addElement = document.querySelector(".create-task-btn");
 
@@ -12,6 +12,7 @@ const addTasks = () => {
     const obj =  {
         text: inputText,
         done: false,
+        date: new Date(),
     };
     tasks.unshift(obj);
     document.querySelector(".task-input").value= "";
@@ -23,6 +24,7 @@ addElement.addEventListener('click', addTasks);
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
     const listItemsElems = listItems
+    .sort((a,b) => b.date - a.date)
     .sort((a,b) => a.done - b.done)
     .map(({text, done}) =>{
         const listItemElem = document.createElement('li');
@@ -48,11 +50,12 @@ const checkBoxToggle = document.querySelector('.list').addEventListener('click',
         event.target.parentElement.classList.toggle('list__item_done');
 
     const stringText = event.target.parentElement.textContent;
-       tasks.map((elem) => {
+      const taskCheck = tasks.map((elem) => {
             if (elem.text === stringText) {
                 elem.done === false ? elem.done = true : elem.done = false;
             }
         }) ;
+        Object.assign(taskCheck, {date: new Date()});
         renderListItems(tasks);
     }
 }); 
